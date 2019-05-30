6fd755982b8019365efad0ee9e14ba6fb910b4ae
sharonsahadevan <sharonsujee36@gmail.com>
2019-05-30 12:02:34 +0530

    missing values added

--- config/default.js
+++ config/default.js
@@ -3,23 +3,23 @@ module.exports = {
 
   "DB": {
     "Type":"postgres",
-    "User":"",
-    "Password":"",
+    "User":"duo",
+    "Password":"DuoS123",
     "Port":5432,
     "Host":"localhost",
-    "Database":""
+    "Database":"dvpdb"
   },
 
 
    "Redis":
     {
         "mode":"sentinel",//instance, cluster, sentinel
-        "ip": "",
+        "ip": "45.55.142.207",
         "port": 6389,
-        "user": "",
-        "password": "",
+        "user": "duo",
+        "password": "DuoS123",
         "sentinels":{
-            "hosts": "",
+            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
             "port":16389,
             "name":"redis-cluster"
         }
@@ -30,13 +30,13 @@ module.exports = {
     "Security":
     {
 
-        "ip" : "",
+        "ip" : "45.55.142.207",
         "port": 6389,
-        "user": "",
-        "password": "",
+        "user": "duo",
+        "password": "DuoS123",
         "mode":"sentinel",//instance, cluster, sentinel
         "sentinels":{
-            "hosts": "",
+            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
             "port":16389,
             "name":"redis-cluster"
         }
@@ -63,7 +63,7 @@ module.exports = {
     "SMSServer":{
 
 
-    "ip":"",
+    "ip":"159.203.109.43",
     "port":"1401",
     "password":"bar",
     "user":"foo"
@@ -74,7 +74,7 @@ module.exports = {
 
     "LBServer" : {
 
-    "ip": "",
+    "ip": "192.168.0.123",
     "port": "4647"
 
   },
@@ -83,10 +83,10 @@ module.exports = {
 
    "SMTP":{
 
-   "ip": "",
+   "ip": "smtp.mandrillapp.com",
    "port": "2525",
-   "user": "",
-   "password": ""
+   "user": "rangika@duosoftware.com",
+   "password": "5eTFMlNGlGnZ7xTmI3LAxQ"
 
    },
 
@@ -96,27 +96,27 @@ module.exports = {
 
   "RabbitMQ":
   {
-    "ip": "",
+    "ip": "45.55.142.207",
     "port": 5672,
-    "user": "",
-    "password": "",
+    "user": "admin",
+    "password": "admin",
     "vhost":'/'
   },
 
   "Mongo":
   {
-    "ip":"",
+    "ip":"104.236.231.11",
     "port":"27017",
-    "dbname":"",
-    "password":"",
-    "user":""
+    "dbname":"dvpdb",
+    "password":"DuoS123",
+    "user":"duo"
   },
 
   "IMAP":
   {
-    "username":"",
-    "password":"",
-    "host":"",
+    "username":"duodemouser@gmail.com",
+    "password":"DuoZ4321",
+    "host":"imap.gmail.com",
     "port":993,
     "secure":true,
     "mailbox":"INBOX",
@@ -132,15 +132,15 @@ module.exports = {
 
 
     "Services" : {
-      "accessToken":"",
+      "accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
 
 
-      "resourceServiceHost": "",
+      "resourceServiceHost": "resourceservice.104.131.67.21.xip.io",
       "resourceServicePort": "8831",
       "resourceServiceVersion": "1.0.0.0",
 
 
-      "interactionurl": "",
+      "interactionurl": "interactions.app.veery.cloud",
       "interactionport": '3637',
       "interactionversion":"1.0.0.0",
 
@@ -150,11 +150,11 @@ module.exports = {
       "cronversion":"1.0.0.0",
 
 
-      "ticketServiceHost": "",//liteticket.app.veery.cloud
+      "ticketServiceHost": "liteticket.app.veery.cloud",//liteticket.app.veery.cloud
       "ticketServicePort": "3636",
       "ticketServiceVersion": "1.0.0.0",
 
-      "ardsServiceHost": "",
+      "ardsServiceHost": "ardsliteservice.104.131.67.21.xip.io",
       "ardsServicePort": "8831",
       "ardsServiceVersion": "1.0.0.0",
 
@@ -164,7 +164,7 @@ module.exports = {
         "mailReceiverPort": "0000",
         "mailReceiverVersion": "1.0.0.0",
 
-        "uploadurl": '',
+        "uploadurl": 'fileservice.app.veery.cloud',
         "uploadport": '8888',
         "uploadurlVersion": "1.0.0.0"
 
